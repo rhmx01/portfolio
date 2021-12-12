44045aa4bc87ddb7590a1202aef1099a2aa27470
Rachid HAMMA ALI <r.hammaali@gmail.com>
2021-12-12 08:53:36 +0100

    Change prod folder to build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Rachid hamma ali</p>
+        <p>Rachid HAMMA ALI</p>
       </header>
     </div>
   );
