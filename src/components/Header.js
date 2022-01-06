5b1b916be3924af8d61948a9195c52db6d9f566e
Rachid HAMMA ALI <r.hammaali@gmail.com>
2022-01-06 08:59:51 +0100

    Header pic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,46 @@
 import React from 'react';
+import {Divider} from "@mui/material";
+import {FacebookOutlined, GitHub, LinkedIn} from "@mui/icons-material";
+import pic from "../images/pic.png"
 
 const Header = () => {
     return (
-        <header id="home" className="h-screen">
+        <header id="home" className="">
 
-            <nav id="nav-wrap">
-
-                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
+            <nav id="nav-wrap" className="w-full flex justify-between">
+                {/*<a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>*/}
+                {/*<a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>*/}
+                <ul className="nav flex items-center">
+                    <li className="logo font-bold text-xl pl-4">Rachid</li>
+                </ul>
                 <ul id="nav" className="nav">
-                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                    <li><a className="smoothscroll" href="#about">About</a></li>
-                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
-                    <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                    <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                    <li className="current"><a className="font-bold" href="#home">Home</a></li>
+                    <li><a className="font-bold" href="#about">About</a></li>
+                    <li><a className="font-bold" href="#resume">Resume</a></li>
+                    <li><a className="font-bold" href="#portfolio">Works</a></li>
+                    <li><a className="font-bold" href="#testimonials">Testimonials</a></li>
+                    <li><a className="font-bold" href="#contact">Contact</a></li>
                 </ul>
 
             </nav>
+            <div className="h-screen flex items-center">
+                <div className="flex items-center grid grid-cols-8 gap-2">
+                    <img src={pic} alt="mypic" className="flex w-80 h-80 rounded-full col-start-2 col-span-2"/>
+                    <div className="col-span-4 text-white">
+                        <h1 className="font-bold header-name">rachid hamma ali.</h1>
+                        <h3 className="text-left">I'm a Ouarzazate based <span>ocupation</span>. I'm a Web Developer with extensive experience for over 5 years. My expertise is to create and Websites design, graphic design and many more...</h3>
+                        <div className="flex">
+                            <FacebookOutlined/>
+                            <GitHub/>
+                            <LinkedIn/>
+                        </div>
+                    </div>
+                    <div>
 
-            <div className="row banner">
-                <div className="banner-text">
-                    <h1 className="responsive-headline">HAMMA ALI RACHID.</h1>
-                    {/*<h3>I'm a Ouarzazate based <span>ocupation</span>. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. Wikipedia .</h3>*/}
-                    {/*<hr />*/}
-                    {/*<ul className="social">*/}
-                    {/*    networks list*/}
-                    {/*</ul>*/}
+                    </div>
                 </div>
             </div>
+
 
             <p className="scrolldown">
                 <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
