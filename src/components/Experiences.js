d593f9c4081b3fea4ab135d3177dfc52846c32af
Rachid HAMMA ALI <r.hammaali@gmail.com>
2022-01-22 21:14:52 +0100

    Add work details

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import work from "../images/work.png";
 import Title from "./Title";
+import {ArrowRight, CalendarToday, Circle, Event, Flag} from "@mui/icons-material";
 
 const Experiences = () => {
     return (
@@ -20,14 +21,23 @@ const Experiences = () => {
                                     <div className="cbp_tmlabel wow fadeInRight animated">
                                         <h3>Project Engineer</h3>
                                         <div className="date">
-                                            <i className="fa fa-calendar"></i>March 2020 - Current
+                                            <Event/> March 2020 - Présent
                                         </div>
-                                        <h4><i className="fa fa-flag"></i>Blue scratch, Casablanca</h4>
+                                        <h4><Flag/> Bluescratch, Casablanca</h4>
                                         <p className="projectParagraph">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Fugit obcaecati ipsa quae, iusto laudantium qui, nisi eum modi
-                                            perspiciatis quasi facilis corporis iure soluta enim incidunt itaque aspernatur
-                                            sequi tempora.
+
+                                            <span className="font-bold"><ArrowRight/>Système d'Information Géographique (SIG) :</span><br/>
+                                            <p className="ml-8">Travailler en équipe pour développer une application SIG avec <b>Laravel</b> & <b>React JS</b>, pour l’Agence Urbaine Ouarzazate-Zagora–Tinghir (AUOZT)</p>
+                                            <br/>
+                                            <span className="font-bold"><ArrowRight/>Learning Management System (LMS) :</span><br/>
+                                            <p className="ml-8">Travail individuel pour développer une application LMS personnalisée avec <b>Moodle</b> pour le Ministère d'Intérieur</p>
+                                            <br/>
+                                            <span className="font-bold"><ArrowRight/>Learning Management System (LMS) :</span><br/>
+                                            <p className="ml-8">Travailler en équipe pour développer une application LMS avancé avec Spring Boot & <b>React JS</b></p>
+
+
+                                            {/*<span className="font-bold"><ArrowRight/>   </span><br/>*/}
+                                            {/*<p className="ml-8">   </p>*/}
                                         </p>
                                     </div>
                                 </li>
