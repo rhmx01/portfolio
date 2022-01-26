fce2d59bf4755e93ba383be417faf434bbfc2751
Rachid HAMMA ALI <r.hammaali@gmail.com>
2022-01-26 09:12:29 +0100

    Skills section

--- src/components/Competances.js
+++ src/components/Competances.js
@@ -9,30 +9,30 @@ const Competances = () => {
         {name: "React js", level: 4},
         {name: "Laravel", level: 4},
         {name: "Spring Boot", level: 3},
-        {name: "AWS", level: 2},
-        {name: "Docker", level: 2},
-        {name: "Moodle", level: 4},
-        {name: "Liferay", level: 2},
-        {name: "DevOps", level: 3},
         {name: "Redux-Saga", level: 4},
-        {name: "Unity 2d", level: 2},
-        {name: "Android Studio", level: 3},
-        {name: "Photoshop", level: 4},
         {name: "Git/Github", level: 4},
         {name: "SQL(mysql, postgresql)", level: 4},
-        {name: "SEO", level: 3},
+        {name: "Android Studio", level: 3},
+        {name: "Moodle", level: 4},
+
+        {name: "Liferay", level: 2},
         {name: "Gestion de projet", level: 2},
+        {name: "SEO", level: 3},
+        {name: "AWS", level: 2},
+        {name: "Docker", level: 3},
+        {name: "DevOps", level: 3},
+        {name: "Unity 2d", level: 2},
+        {name: "Photoshop", level: 4},
     ];
     return (
-        <div className="mt-5 pt-8">
+        <div id="skills" className="mt-5 pt-8">
             <div className=" mt-5 mb-5">
                 <Title title="Compétences" icon={work} color="#fff"/>
             </div>
 
-
-            <div className="">
+            <div className="grid grid-cols-2 gap-1 mx-auto">
                 {skills.map( skill => (
-                    <div className="grid grid-cols-4 gap-4 items-center text-white">
+                    <div className="flex justify-between w-96 items-center text-white border-b mt-4 mx-auto">
                         <h1 className="font-bold text-xl mr-2">{skill.name}</h1>
                         <Rating
                             name="read-only"
